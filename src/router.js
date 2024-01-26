c9e48164fa39269b7f19c33f58c56d574fe80d65
Wai Yan Latt <108973955+WaiYanLatt@users.noreply.github.com>
2024-01-26 09:32:25 +0630

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
+   {
+         path : '/',
+         redirect : '/'
+   },
   {
     path: "/",
     name: "Login",
